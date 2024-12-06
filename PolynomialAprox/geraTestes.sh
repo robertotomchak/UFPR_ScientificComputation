a111df25b5ad56969805d176c9b5d96e2672645c
robertotomchak <robertotomchak@gmail.com>
2024-12-06 11:55:16 -0300

    add stuff

--- PolynomialAprox/geraTestes.sh
+++ PolynomialAprox/geraTestes.sh
@@ -4,19 +4,50 @@ K="64 128 200 256"
 n=256
 METRICAS="L3CACHE ENERGY FLOPS_DP"
 
-mkdir results
+if [ ! -d "results" ]; then
+    mkdir results
+fi
+if [ ! -d "results/v1" ]; then
+    mkdir results/v1
+fi
+if [ ! -d "results/v2" ]; then
+    mkdir results/v2
+fi
 
-for metrica in $METRICAS
+echo "n,k,version,time1,time2" > "results/time.csv"
+
+for k in $K
 do
-    for k in $K
+    ./gera $k $n > entrada.in
+    # getting time (no need for likwid)
+    ./ajustePolV1 < entrada.in | tail -n1 > temp.txt
+    tempo_monta=$(cut -d' ' temp.txt -f2)
+    tempo_elimina=$(cut -d' ' temp.txt -f3)
+    echo $n","$k",1,"$tempo_monta","$tempo_elimina >> results/time.csv
+    ./ajustePolV2 < entrada.in | tail -n1 > temp.txt
+    tempo_monta=$(cut -d' ' temp.txt -f2)
+    tempo_elimina=$(cut -d' ' temp.txt -f3)
+    echo $n","$k",2,"$tempo_monta","$tempo_elimina >> results/time.csv
+    for metrica in $METRICAS
     do
-        temp_in=$(./gera $k $n)
-        nome_arq=($metrica"_"$k"_"$n".txt")
+        nome_arq=($metrica"_"$k"_"$n".csv")
         echo $nome_arq
-        echo $temp_in | likwid-perfctr -O -C 3 -g $metrica -m ./ajustePolV2 > temp.txt
+
+        # results for v1
+        cat entrada.in | likwid-perfctr -O -C 3 -g $metrica -m ./ajustePolV1 > temp.txt
         tam=$(wc -l temp.txt | cut -d' ' -f1)
         inicio=$(grep '\-\-' temp.txt -n | tail -n1 | cut -d':' -f1)
         pegar=$(($tam-$inicio))
-        tail -n$pegar temp.txt > results/$nome_arq
+        tail -n$pegar temp.txt > results/v1/$nome_arq
+
+        # results for v2
+        cat entrada.in | likwid-perfctr -O -C 3 -g $metrica -m ./ajustePolV2 > temp.txt
+        tam=$(wc -l temp.txt | cut -d' ' -f1)
+        inicio=$(grep '\-\-' temp.txt -n | tail -n1 | cut -d':' -f1)
+        pegar=$(($tam-$inicio))
+        tail -n$pegar temp.txt > results/v2/$nome_arq
     done
 done
+
+rm temp.txt
+rm entrada.in
