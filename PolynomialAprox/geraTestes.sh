e7d96b4f77e8c10f7d80f80da9df3ade540e1c5f
robertotomchak <robertotomchak@gmail.com>
2024-12-06 10:09:56 -0300

    Add generic test

--- PolynomialAprox/geraTestes.sh
+++ PolynomialAprox/geraTestes.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 K="64 128 200 256"
-n=1024
+n=256
+METRICAS="L3CACHE ENERGY FLOPS_DP"
 
-for k in $K
+mkdir results
+
+for metrica in $METRICAS
 do
-    temp_in=$(./gera $k $n)
-    echo $temp_in | likwid-perfctr -O -C 3 -g ENERGY -m ./ajustePolV2 > temp.txt
-    tail temp.txt -n10
+    for k in $K
+    do
+        temp_in=$(./gera $k $n)
+        nome_arq=($metrica"_"$k"_"$n".txt")
+        echo $nome_arq
+        echo $temp_in | likwid-perfctr -O -C 3 -g $metrica -m ./ajustePolV2 > temp.txt
+        tam=$(wc -l temp.txt | cut -d' ' -f1)
+        inicio=$(grep '\-\-' temp.txt -n | tail -n1 | cut -d':' -f1)
+        pegar=$(($tam-$inicio))
+        tail -n$pegar temp.txt > results/$nome_arq
+    done
 done
